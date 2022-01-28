3b3a43fca0f31084b47144978e9fca5fae015d1c
Anne Ensign <anne.ensign@gmail.com>
2022-01-28 11:28:28 -0500

    SQL search by parameters

--- stock-market/python/get_records.py
+++ stock-market/python/get_records.py
@@ -3,7 +3,16 @@ from os import path
 
 conn = sqlite3.connect('../sql/sample.db')
 c = conn.cursor()
-c.execute('SELECT email FROM users')
+# c.execute('SELECT email, last, first FROM users ORDER BY last')
 
+# for row in c.fetchall():
+# 	print(row[0], row[1], row[2])
+
+# Using a parameter, last=? which looks for x
+x = 'Kirk','Chan',
+c.execute('SELECT email,last,first FROM users WHERE last=? OR last=?',x)
 for row in c.fetchall():
-	print(row[0])
+	print(row[0], row[1], row[2])
+
+conn.commit()
+conn.close()
