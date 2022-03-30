d7aedf31c520b5ad46526c6a0f15c53564a08790
Anne Ensign <anne.ensign@gmail.com>
2022-03-30 16:16:34 -0400

    Add test cases

--- unittest/phonenumbers/test_phonebook.py
+++ unittest/phonenumbers/test_phonebook.py
@@ -4,12 +4,24 @@ from phonebook import PhoneBook
 
 class PhoneBookTest(unittest.TestCase):
 
-	def test_lookup_by_name(self):
-		phonebook = PhoneBook()
-		phonebook.add("Bob", "12345")
-		number = phonebook.lookup("Bob")
-		self.assertEqual("12345", number)
+	def setUp(self) -> None:
+		self.phonebook = PhoneBook()
+	
+	def tearDown(self) -> None:
+		pass
 
+	def test_lookup_by_name(self):
+		self.phonebook.add("Bob", "12345")
+		number = self.phonebook.lookup("Bob")
+		self.assertEqual("12345", number)
+	
+	def test_missing_name(self):
+		with self.assertRaises(KeyError):
+			self.phonebook.lookup("missing")
+
+	@unittest.skip("WIP")
+	def test_empty_phonebook_is_consistant(self):
+		self.assertTrue(self.phonebook.is_consistent())
 
 if __name__ == '__main__':
 	unittest.main()
